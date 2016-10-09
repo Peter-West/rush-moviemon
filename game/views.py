85c29eb96ba57af65752ac355078265efbf7034d
Adrien PACHKOFF <apachkof@student.42.fr>
2016-10-09 15:08:35 +0200

    created all urls and (dumb) views + empty css file

--- game/views.py
+++ game/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+def title_screen(request):
+    context = {}
+    return render(request, "game/title_screen.html", context)
+
+def worldmap(request):
+    context = {}
+    return render(request, "game/worldmap.html", context)
+
+def battle(request):
+    context = {}
+    return render(request, "game/battle.html", context)
+
+def battle_moviemon(request, moviemon_id):
+    context = {"moviemon_id": moviemon_id}
+    return render(request, "game/battle_moviemon.html", context)
+
+def moviedex(request):
+    context = {}
+    return render(request, "game/moviedex.html", context)
+
+def moviedex_moviemon(request, moviemon_id):
+    context = {"moviemon_id": moviemon_id}
+    return render(request, "game/moviedex_moviemon.html", context)
+
+def options(request):
+    context = {}
+    return render(request, "game/options.html", context)
+
+def options_save_game(request):
+    context = {}
+    return render(request, "game/options_save_game.html", context)
+
+def options_load_game(request):
+    context = {}
+    return render(request, "game/options_load_game.html", context)
+
